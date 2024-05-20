a63c05759be3d391d3c0123ce20f7a260bf94461
lxh <1627812101@qq.com>
2024-05-20 22:58:45 +0800

    :fire:  合并了下代码

--- module_schoolroom/build.gradle.kts
+++ module_schoolroom/build.gradle.kts
@@ -52,7 +52,6 @@ dependencies {
     annotationProcessor("androidx.room:room-compiler:2.6.1")
     testImplementation ("androidx.room:room-testing:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
 
 
 
